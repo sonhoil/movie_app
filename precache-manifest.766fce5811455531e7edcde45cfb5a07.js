e47873f90b4cb258428b8c82fd9eb13ed83e50d7
sonhoil <som6642@naver.com>
2020-03-22 16:24:01 +0900

    Updates

---
+++ precache-manifest.766fce5811455531e7edcde45cfb5a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea88f03b22fa37663f576d5afeb33b2e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ed7012ad60fc9e746e88",
+    "url": "/movie_app/static/css/main.a395a0c3.chunk.css"
+  },
+  {
+    "revision": "d927bcd77cdb596b8a59",
+    "url": "/movie_app/static/js/2.9c221012.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.9c221012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7012ad60fc9e746e88",
+    "url": "/movie_app/static/js/main.f76d734c.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
